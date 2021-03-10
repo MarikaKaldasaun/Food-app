ff18f86b1bddd57ed2f3af6f854073da68dba538
Marika Kaldasaun <marika.kaldasaun@gmail.com>
2021-03-10 15:21:21 +0200

    moved some rows

--- src/App.js
+++ src/App.js
@@ -18,19 +18,22 @@ function App() {
   const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
+
+    const getRecipes = async () => {
+      const response = await fetch(
+        'https://api.edamam.com/search?q=' +query+ '&app_id=' +APP_ID+ '&app_key=' +APP_KEY                 
+  
+      );
+  
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    };
+
     getRecipes();
   }, [query]);
 
-  const getRecipes = async () => {
-    const response = await fetch(
-      'https://api.edamam.com/search?q=' +query+ '&app_id=' +APP_ID+ '&app_key=' +APP_KEY                 
 
-    );
-
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  };
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
@@ -67,7 +70,7 @@ function App() {
     value={value}
     onChange={val => setValue}>
      </Dropdown>
- </div>
+     </div>
     
   
       <div className="recipes">
